82d0c63cd79c6a0240294a93efd8bb00a46614c2
Roberto Ramirez <robert.624@hotmail.com>
2023-09-01 12:23:32 -0500

    feat: only logged in users can access current user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import validateResource from '../middleware/validateResource';
 import { createUserSchema, forgotPasswordSchema, resetPasswordSchema, verifyUserSchema } from '../schema/user.schema';
 import { createUserHandler, forgotPasswordHandler, getCurrentUserHandler, resetPasswordHandler, verifyUserHandler } from '../controllers/user.controller';
+import requireUser from '../middleware/requireUser';
 
 const router = express.Router();
 
@@ -15,6 +16,6 @@ router.post('/users/forgotpassword', validateResource(forgotPasswordSchema), for
 
 router.post('/users/resetpassword/:id/:passwordResetCode', validateResource(resetPasswordSchema), resetPasswordHandler)
 
-router.get('/users/me', getCurrentUserHandler)
+router.get('/users/me', requireUser, getCurrentUserHandler)
 
 export default router;
